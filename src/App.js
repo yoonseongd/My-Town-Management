9a0889c6185c881e4a918cec40d02b31a87251fc
yoonseongd <dys0716@gmail.com>
2020-07-05 16:09:28 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Customer from "./components/Customer";
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} alt="logo" />
-      <h2>개발하자</h2>
-    </div>
-  );
+const customer = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/any",
+    name: "adsf",
+    birthday: "111",
+    gender: "2",
+    job: "asdf",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/any",
+    name: "asdfasdf",
+    birthday: "352",
+    gender: "asdf",
+    job: "qtqwr",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/any",
+    name: "zxcv",
+    birthday: "djh",
+    gender: "87",
+    job: "mvb",
+  },
+];
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        {customer.map((c) => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
