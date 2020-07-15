317ee1a37649f75e2101f05a3566fe6f8027cc7f
yoonseongd <53684010+yoonseongd@users.noreply.github.com>
2020-07-15 14:24:25 +0900

    Implement Customer Add Module & Customer List Refresh Module

--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -8,8 +8,13 @@ class Customer extends React.Component {
       <TableRow>
         <TableCell>{this.props.id}</TableCell>
         <TableCell>
-          <img src={this.props.image} alt="profile" />
+          <img
+            src={this.props.image}
+            alt="profile"
+            style={{ width: 64, height: 64 }}
+          />
         </TableCell>
+
         <TableCell>{this.props.name}</TableCell>
         <TableCell>{this.props.birthday}</TableCell>
         <TableCell>{this.props.gender}</TableCell>
--- server.js
+++ server.js
@@ -21,11 +21,27 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
+const multer = require("multer");
+const upload = multer({ dest: "./upload" });
+
 //고객 목록을 불러주는 custorms api
 app.get("/api/customers", (req, res) => {
   connection.query("select * from customer", (err, rows, fields) => {
     res.send(rows);
   });
 });
-
+app.use("/image", express.static("./upload"));
+app.post("/api/customers", upload.single("image"), (req, res) => {
+  let sql = "insert into customer values(null,?,?,?,?,?)";
+  // let image = "/image" + req.file.filename;
+  let image = "http://localhost:5000/image/" + req.file.filename;
+  let name = req.body.name;
+  let birthday = req.body.birthday;
+  let gender = req.body.gender;
+  let job = req.body.job;
+  let params = [image, name, birthday, gender, job];
+  connection.query(sql, params, (err, rows, fields) => {
+    res.send(rows);
+  });
+});
 app.listen(port, () => console.log(`listening on port  ${port}`));
