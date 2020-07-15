ea7bc4bc2a9e6ccf64824630f1007cf6cf50020c
yoonseongd <53684010+yoonseongd@users.noreply.github.com>
2020-07-15 10:21:41 +0900

    first commit

---
+++ client/src/components/Customer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import TableCell from "@material-ui/core/TableCell";
+import TableRow from "@material-ui/core/TableRow";
+
+class Customer extends React.Component {
+  render() {
+    return (
+      <TableRow>
+        <TableCell>{this.props.id}</TableCell>
+        <TableCell>
+          <img src={this.props.image} alt="profile" />
+        </TableCell>
+        <TableCell>{this.props.name}</TableCell>
+        <TableCell>{this.props.birthday}</TableCell>
+        <TableCell>{this.props.gender}</TableCell>
+        <TableCell>{this.props.job}</TableCell>
+      </TableRow>
+    );
+  }
+}
+
+export default Customer;
