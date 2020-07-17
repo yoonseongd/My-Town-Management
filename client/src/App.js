01e782e33f27fdfd25046b2ee7024ec586e3dfb1
yoonseongd <53684010+yoonseongd@users.noreply.github.com>
2020-07-17 11:50:02 +0900

    Add AppBar & WebFont

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,112 @@
-import React from "react";
+import React, { Component } from "react";
+import Customer from "./components/Customer";
 import "./App.css";
 import Paper from "@material-ui/core/Paper";
-import Customer from "./components/Customer";
-import CustomerAdd from "./components/CustomerAdd";
 import Table from "@material-ui/core/Table";
 import TableHead from "@material-ui/core/TableHead";
 import TableBody from "@material-ui/core/TableBody";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
-import { withStyles } from "@material-ui/core/styles";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import { withStyles } from "@material-ui/core/styles";
+import CustomerAdd from "./components/CustomerAdd";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import InputBase from "@material-ui/core/InputBase";
+import { fade } from "@material-ui/core/styles/colorManipulator";
+import Typography from "@material-ui/core/Typography";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
 
 const styles = (theme) => ({
   root: {
     width: "100%",
-    marginTop: theme.spacing(3),
-    overflowX: "auto",
-  },
-  table: {
     minWidth: 1080,
   },
+  menu: {
+    marginTop: 15,
+    marginBottom: 15,
+    display: "flex",
+    justifyContent: "center",
+  },
+  paper: {
+    marginLeft: 18,
+    marginRight: 18,
+  },
   progress: {
-    margin: theme.spacing(2),
+    margin: theme.spacing.unit * 2,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  tableHead: {
+    fontSize: "1.0rem",
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  title: {
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing.unit,
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+    width: "100%",
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: 120,
+      "&:focus": {
+        width: 200,
+      },
+    },
   },
 });
 
-class App extends React.Component {
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       customers: "",
       completed: 0,
     };
+    this.stateRefresh = this.stateRefresh.bind(this);
   }
-  stateRefresh = () => {
+
+  stateRefresh() {
     this.setState({
       customers: "",
       completed: 0,
@@ -41,13 +114,17 @@ class App extends React.Component {
     this.callApi()
       .then((res) => this.setState({ customers: res }))
       .catch((err) => console.log(err));
-  };
+  }
 
   componentDidMount() {
     this.timer = setInterval(this.progress, 20);
     this.callApi()
       .then((res) => this.setState({ customers: res }))
       .catch((err) => console.log(err));
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
   }
 
   callApi = async () => {
@@ -63,19 +140,61 @@ class App extends React.Component {
 
   render() {
     const { classes } = this.props;
+    const cellList = [
+      "번호",
+      "프로필 이미지",
+      "이름",
+      "생년월일",
+      "성별",
+      "직업",
+      "설정",
+    ];
     return (
-      <div>
-        <Paper className={classes.root}>
-          <Table className={classes.table}>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="Open drawer"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              className={classes.title}
+              variant="h6"
+              color="inherit"
+              noWrap
+            >
+              고객 관리 시스템
+            </Typography>
+            <div className={classes.grow} />
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="검색하기"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+              />
+            </div>
+          </Toolbar>
+        </AppBar>
+        <div className={classes.menu}>
+          <CustomerAdd stateRefresh={this.stateRefresh} />
+        </div>
+        <Paper className={classes.paper}>
+          <Table>
             <TableHead>
               <TableRow>
-                <TableCell>번호</TableCell>
-                <TableCell>이미지 </TableCell>
-                <TableCell>이름</TableCell>
-                <TableCell>생년월일</TableCell>
-                <TableCell>성별</TableCell>
-                <TableCell>직업</TableCell>
-                <TableCell>설정</TableCell>
+                {cellList.map((c) => {
+                  return (
+                    <TableCell className={classes.tableHead}>{c}</TableCell>
+                  );
+                })}
               </TableRow>
             </TableHead>
             <TableBody>
@@ -108,7 +227,6 @@ class App extends React.Component {
             </TableBody>
           </Table>
         </Paper>
-        <CustomerAdd stateRefresh={this.stateRefresh} />
       </div>
     );
   }
